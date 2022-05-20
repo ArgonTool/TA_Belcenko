94ea078006d79518d50b86c67d140b29ec279c15
ArgonTool <55786033+ArgonTool@users.noreply.github.com>
2022-05-20 21:10:12 +0200

    more code

--- src/Map.java
+++ src/Map.java
@@ -1,7 +1,40 @@
-public class Map {
+import java.io.Serializable;
+
+public class Map implements Serializable{
     Tile[][] arr;
+    int size = 40;
 
     public void generateMap(){
-
+        arr = new Tile[size/2][size];
+        for (int i = 0; i < size/2; i++) {
+            for (int j = 0; j < size; j++) {
+                arr[i][j] = new Tile(Tile.Type.NOTHING);
+            }
+        }
     }
+
+    public void render(){
+        StringBuffer buff = new StringBuffer();
+        for (Tile[] row : arr) {
+            for (Tile column: row) {
+                try {
+                    buff.append(column.getIcon());
+                }catch(NullPointerException e){
+                    System.out.println("ERROR: MAP GENERATION");
+                    return;
+                }
+            }
+            buff.append('\n');
+        }
+        System.out.println(buff.toString());
+    }
+
+    /*public static void serializeDataOut()throws IOException {
+        String fileName= "map.txt";
+        FileOutputStream fos = new FileOutputStream(fileName);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject();
+        oos.close();
+    }*/
+
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,58 @@
-public class Tile {
+import java.io.Serializable;
+
+public class Tile implements Serializable{
+
+    enum Type{
+        TRAP,
+        CHEST,
+        ENTRANCE,
+        EXIT,
+        NOTHING,
+        WALL
+    }
+    private Type tileType;
+
+    enum Status{
+        EXPLORED,
+        NOT_EXPLORED
+    }
+
+    private Status status;
+    private String text;
+    private char icon;
+
+    public Tile(Type tileType) {
+        this.tileType = tileType;
+        switch (tileType){
+            case TRAP -> {
+                this.text = "";
+                this.icon = 'o';
+            }
+            case CHEST -> {
+                this.text = "";
+                this.icon = 'c';
+            }
+            case ENTRANCE -> {
+                this.text = "";
+            }
+            case EXIT -> this.text = "";
+            case NOTHING -> {
+                this.text ="Nothing interesting";
+                this.icon = '+';
+            }
+            case WALL -> {
+                this.text="You shouldn't be in  a wall";
+                this.icon = '0';
+            }
+        }
+        this.status = Status.NOT_EXPLORED;
+    }
+
+    public char getIcon() {
+        return icon;
+    }
+
+    public String getText(){
+        return null;
+    }
 }
